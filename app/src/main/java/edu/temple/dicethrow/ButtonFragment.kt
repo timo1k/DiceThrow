c7a891670791380beb13e1aa8f166a8af39f2bbf
Timothy <timkhumpan@gmail.com>
2024-10-22 15:23:05 -0400

    create button fragment implementation

---
+++ app/src/main/java/edu/temple/dicethrow/ButtonFragment.kt
@@ -0,0 +1,37 @@
+package edu.temple.dicethrow
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+
+class ButtonFragment : Fragment() {
+    lateinit var button: Button
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_button, container, false).apply {
+            button = findViewById(R.id.button)
+            button.setOnClickListener{
+                //increased coupling
+                //define a interface with parents functionality - contract
+                (requireActivity() as MainActivity).buttonClick()
+
+                (requireActivity() as ButtonInterface).buttonClick()
+            }
+
+        }
+    }
+
+}
+
+interface ButtonInterface {
+    fun buttonClick(){}
+}
--- app/src/main/java/edu/temple/dicethrow/MainActivity.kt
+++ app/src/main/java/edu/temple/dicethrow/MainActivity.kt
@@ -8,6 +8,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    fun buttonClick(){
+        (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -16,18 +21,19 @@ class MainActivity : AppCompatActivity() {
 
         //ffree fragment manager but now we use this we have to impleement saving state itself
 
+
         if (supportFragmentManager.findFragmentById(R.id.dieContainer) !is DieFragment)
             supportFragmentManager
                 .beginTransaction()
                 .add(R.id.dieContainer, fragment1)
                 .commit()
 
-        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
-
-            //returns ref of type fragment but we need to access the dieFrag
-            //type cast
-            (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
-        }
+//        findViewById<Button>(R.id.rollDiceButton).setOnClickListener {
+//
+//            //returns ref of type fragment but we need to access the dieFrag
+//            //type cast
+//            (supportFragmentManager.findFragmentById(R.id.dieContainer) as DieFragment).throwDie()
+//        }
 
     }
 }
